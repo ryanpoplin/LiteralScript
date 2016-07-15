b513b5062a6d2dcd8685190074a0c4d352b8b6f1
Ryan Poplin <ryan.poplin@turner.com>
2016-07-14 22:46:33 -0400

    init: Setup repo and add first file
    I have added a concept file of the language in conjunction with
    JavaScript.
    
    Reviewed-By: Ryan Poplin

---
+++ liter@lscript.js
@@ -0,0 +1,43 @@
+/* Liter@lScript */
+
+// # auto constants
+
+// f@{Page(content)[
+//     p@content
+// ]}
+const Page = (content) => {
+    this.content = content;
+};
+
+// # a comment for Liter@lScript
+// # a multi-line
+// comment for Liter@lScript #
+
+// f@{Page}proto@{[
+//     m@render() <- '<div>t@p@content@t</div>'
+// ]}
+Page.prototype.render = function () {
+    return `<div>${this.content}</div>`;
+};
+
+// f@{Post(tags, content)[
+//     p@tags
+//     # figure this syntax out
+//     c@f@{Page}|content|
+// ]}
+const Post = (tags, content) => {
+    this.tags = tags;
+    // what's this doing for me?
+    Page.call(this, content);
+};
+
+// f@{Post}proto@{[
+//     init@{Page('initialize a Page function object')}
+// ]}
+Post.prototype = new Page('new Page');
+
+// post <= init@{Post(['good', 'bad', 'ugly'], 'Here is some awesome content...')}
+const post = new Post(['good', 'bad', 'ugly'], 'Here is some awesome content...');
+
+// log@f@{post}
+console.log(post);
